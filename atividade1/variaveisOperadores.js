11b63126258d8868ea61939edeb4ecbb7216b6c2
EMILLY VICTORIA DA SILVA PEIXOTO <emilly.peixoto@a.ucb.br>
2023-08-10 11:03:54 -0300

    alteração no print do resultado

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -1,8 +1,13 @@
-var a = 2;
-var b = 10;
+var a = 10;
+var b = 2;
 
 var soma = a + b;
 var subtracao = a - b;
 var multiplicacao  = a * b;
+var divisao = a / b;
 
-console.log ("O resultado da operação é: " + soma)
+
+console.log ("O resultado da operação de soma entre " + a + " + " + b + " é igual a = " + soma);
+console.log ("O resultado da operação de subtração entre " + a + " - " + b + " é igual a = " + subtracao);
+console.log ("O resultado da operação de multiplicação entre " + a + " * " + b + " é igual a = " + multiplicacao);
+console.log ("O resultado da operação de divisão entre " + a + " / " + b + " é igual a = " + divisao);
