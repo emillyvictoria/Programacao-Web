0ceb0e34d15b821c9c2cb7d1a3a4ebe1ff59285b
emillyvictoria <emillyvsp02@gmail.com>
2023-08-24 10:08:43 -0300

    update

--- atividade2/exercicio2.js
+++ atividade2/exercicio2.js
@@ -35,3 +35,34 @@ function printMatrix(matrix) {
   printMatrix(transposedA);
   
 
+/*  let a = [[1,2],
+           [3,4],
+           [5,6]];
+
+function mudaPosicao (a) {
+  b = [
+    [a[0][0], a[1][0], a[2][0]],
+    [a[0][1], a[1][1], a[2][1]],    
+  ]
+  return b;
+}
+
+function mudaPosicao2 (matriz) {
+  let qtdL = matriz.lenght;
+  let qtdC = matriz [0].lenght;
+  let b = Array (qtdC);
+  for (let i = 0; i < 2; i++){
+    b[i] = Array(qtdC);
+  }
+
+  for (let i = 0; i < qtdL; i++) {
+    for (let j = 0; j < qtdC; j++) {
+        b[j][i] = matriz [i][j];
+    }
+  }
+  return b;
+}
+
+b = mudaPosicao2 (a);
+
+console.log("Matriz resultante C:", matriz)*/
